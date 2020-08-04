93c1d1c86781f355629694b4b5b887096d14b819
minginggg <mingyeongi147@gmail.com>
2020-08-04 10:13:11 +0900

    crawling

--- movie_review.py
+++ movie_review.py
@@ -9,12 +9,12 @@ soup = BeautifulSoup(response.text,"html.parser")
 
 movie_list = soup.select("dt[class=tit] > a")
 
-cut_movie=dict()
+cut_movie=[]
 count=0
 for i in movie_list:
     link = i.get("href").split('code=')[1]
     name = i.text
-    cut_movie[count] = {"title":name, "code":link}
+    cut_movie.append({"title":name, "code":link})
     count+=1
 
 print(cut_movie)
